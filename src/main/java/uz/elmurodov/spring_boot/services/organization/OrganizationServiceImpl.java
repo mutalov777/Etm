58dd90f7d85038b10814cc81363d9e649a375f00
jasur <jasurmutalov2001014@gmail.com>
2022-02-17 13:15:58 +0500

    create project for testing

--- src/main/java/uz/elmurodov/spring_boot/services/organization/OrganizationServiceImpl.java
+++ src/main/java/uz/elmurodov/spring_boot/services/organization/OrganizationServiceImpl.java
@@ -1,15 +1,21 @@
 package uz.elmurodov.spring_boot.services.organization;
 
+import lombok.NonNull;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import uz.elmurodov.spring_boot.criteria.GenericCriteria;
 import uz.elmurodov.spring_boot.dto.organization.OrganizationCreateDto;
 import uz.elmurodov.spring_boot.dto.organization.OrganizationDto;
 import uz.elmurodov.spring_boot.dto.organization.OrganizationUpdateDto;
+import uz.elmurodov.spring_boot.entity.file.Uploads;
 import uz.elmurodov.spring_boot.entity.organization.Organization;
 import uz.elmurodov.spring_boot.mapper.OrganizationMapper;
 import uz.elmurodov.spring_boot.reposiroty.OrganizationRepository;
 import uz.elmurodov.spring_boot.services.AbstractService;
+import uz.elmurodov.spring_boot.services.file.FileStorageService;
 import uz.elmurodov.spring_boot.utils.BaseUtils;
 import uz.elmurodov.spring_boot.utils.validators.organization.OrganizationValidator;
 
@@ -20,17 +26,25 @@ public class OrganizationServiceImpl extends AbstractService<OrganizationReposit
         OrganizationMapper, OrganizationValidator>
         implements OrganizationService {
 
-
+    private final FileStorageService fileStorageService;
     @Autowired
-    protected OrganizationServiceImpl(OrganizationRepository repository, OrganizationMapper mapper, OrganizationValidator validator, BaseUtils baseUtils) {
+    protected OrganizationServiceImpl(OrganizationRepository repository,  OrganizationMapper mapper, OrganizationValidator validator, BaseUtils baseUtils, FileStorageService fileStorageService) {
         super(repository, mapper, validator, baseUtils);
+        this.fileStorageService = fileStorageService;
     }
 
     @Override
     public Long create(OrganizationCreateDto createDto) {
-        Organization organization = mapper.fromCreateDto(createDto);
+        Organization organization = createPath(createDto,createDto.getLogo());
         repository.save(organization);
         return organization.getId();
+    }
+    @SneakyThrows
+    public Organization createPath(OrganizationCreateDto dto, @NonNull MultipartFile file) {
+        Organization organization = mapper.fromCreateDto(dto);
+        Uploads uploads = fileStorageService.store(file);
+        organization.setLogo(uploads.getPath());
+        return organization;
     }
 
     @Override
--- src/main/java/uz/elmurodov/spring_boot/services/project/ProjectServiceImpl.java
+++ src/main/java/uz/elmurodov/spring_boot/services/project/ProjectServiceImpl.java
@@ -3,6 +3,7 @@ package uz.elmurodov.spring_boot.services.project;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import uz.elmurodov.spring_boot.criteria.GenericCriteria;
@@ -14,6 +15,7 @@ import uz.elmurodov.spring_boot.entity.project.Project;
 import uz.elmurodov.spring_boot.mapper.ProjectMapper;
 import uz.elmurodov.spring_boot.reposiroty.ProjectRepository;
 import uz.elmurodov.spring_boot.services.AbstractService;
+import uz.elmurodov.spring_boot.services.file.FileStorageService;
 import uz.elmurodov.spring_boot.utils.BaseUtils;
 import uz.elmurodov.spring_boot.utils.validators.project.ProjectValidator;
 
@@ -22,9 +24,11 @@ import java.util.List;
 @Service
 public class ProjectServiceImpl extends AbstractService<ProjectRepository, ProjectMapper, ProjectValidator>
         implements ProjectService {
+    private final FileStorageService fileStorageService;
     @Autowired
-    protected ProjectServiceImpl(ProjectRepository repository, ProjectMapper mapper, ProjectValidator validator, BaseUtils baseUtils) {
+    protected ProjectServiceImpl(ProjectRepository repository, ProjectMapper mapper, ProjectValidator validator, BaseUtils baseUtils, FileStorageService fileStorageService) {
         super(repository, mapper, validator, baseUtils);
+        this.fileStorageService = fileStorageService;
     }
 
     @Override
